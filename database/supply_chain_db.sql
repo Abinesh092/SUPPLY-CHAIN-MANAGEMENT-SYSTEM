f5cf8fdbb86860f21197c08bb59e7c49eb99b158
Abinesh092 <abinesharunachalam03@gmail.com>
2026-05-02 14:07:29 +0530

    Update supply_chain_db.sql

--- database/supply_chain_db.sql
+++ database/supply_chain_db.sql
@@ -6,8 +6,7 @@ CREATE TABLE suppliers (
     supplier_name VARCHAR(255) NOT NULL,
     contact_info VARCHAR(255)
 );
-//h
-    
+
 CREATE TABLE products (
     product_id INT AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(255) NOT NULL,
